d6da80419161086b63d835082cfcc1357481ca94
diarisdiakite <diarisdiakite@gmail.com>
2023-04-14 03:52:54 +0200

    Add javascript list of projects object array and logic - to be fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,142 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburgerNavMenu.classList.remove('active');
   // hamburgerNavMenu.classList.remove("showHamburgerNavMenu");
 }));
+
+
+// POPUP WINDOW
+
+
+//let projectPopup = document.getElementById('projectPopup');
+let projects = [
+    {
+    title: 'Mobile version skeleton',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  },
+  {
+    title: 'Mobile version',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  },
+  {
+    title: 'Contact form',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  },
+  {
+    title: 'Desktop version',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  },
+  {
+    title: 'Application deploy',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  },
+  {
+    title: 'Evaluate accessibility',
+    technologies: [
+      'html',
+      'javascript',
+      'css',
+      'React Js'
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    liveVersionLink: 'live-version-link',
+    sourceLink: 'source-link'
+  }
+];
+
+//const container = document.getElementById('recent_work');
+
+  projects.forEach(project => {
+  // Create card element
+  const projectCardJs = document.createElement('div');
+  projectCardJs.classList = 'open-project-popup';
+ 
+        
+  //Project technology list
+  //let Navmenu = document.createElement('ul');
+    
+  //appending image
+  let image = document.createElement('img');
+  image.src = '../../images/popup/SnapshootPortfolio.svg';
+  image.classList = 'project-popup-img';
+  projectCardJs.appendChild(image);
+
+  //Project Title
+  let title = document.createElement('h3');
+  title.textContent = `${project.title}`;
+  title.classList = "section-title"
+  projectCardJs.appendChild(title);
+    
+  //Project links
+  let liveLink = document.createElement('a');
+  liveLink.text = `${project.liveVersionLink}`;
+  projectCardJs.appendChild(liveLink)
+
+  let sourceLink = document.createElement('a');
+  sourceLink.text = `${project.sourceLink}`;
+  projectCardJs.appendChild(sourceLink);
+
+  let listCompetences = document.createDocumentFragment();
+  technologies.forEach(technology => {
+    let li = document.createElement('li');
+    li.textContent = `${project.technologies[technology]}`;
+    li.classList = 'project-built-with > li';
+    listCompetences.appendChild(li);
+  })
+  projectCardJs.appendChild(listCompetences);
+
+  //Project description
+  let p = document.createElement('p');
+  p.textContent = `${project.description}`
+  projectCardJs.appendChild(p);
+
+  let projectPopup = document.querySelector("#projectPopup");
+  projectPopup.appendChild(projectCardJs);
+})
+
+
+const openProjectPopup = () => { //Testing the other function
+  projectPopup.classList.add('open-project-popup')
+}
+
+const closeProjectPopup = () => {
+  projectPopup.classList.remove('open-project-popup');
+}
