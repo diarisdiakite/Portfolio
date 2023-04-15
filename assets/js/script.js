3118f23cea3e85eb2342851e35dff1c0a6f0e01e
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-15 07:13:57 +0300

    Add toggle for menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerNavMenu = document.querySelector('.hamburger-navmenu');
+const navbar = document.querySelector('.not-navbar')
 
 hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active');
-  hamburgerNavMenu.classList.toggle('active');
+  if (navbar.classList.contains('hidden')) {
+    navbar.classList.remove('hidden')
+  } else {
+    navbar.classList.add('hidden');
+  }
+
+  if (hamburgerNavMenu.classList.contains('hidden')) {
+    hamburgerNavMenu.classList.remove('hidden')
+  } else {
+    hamburgerNavMenu.classList.add('hidden');
+  }
+ // hamburgerMenu.classList.toggle('active');
+  //hamburgerNavMenu.classList.toggle('active');
   // hamburgerNavMenu.classList.toggle("showHamburgerNavMenu");
 });
 
