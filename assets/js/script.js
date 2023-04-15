036293b3e3d70d87dce9a598e661f57ef73dd41e
diarisdiakite <diarisdiakite@gmail.com>
2023-04-15 19:51:28 +0200

    Rewrite the js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.setAttribute('class', 'hamburger-menu');
+
 const hamburgerNavMenu = document.querySelector('.hamburger-navmenu');
+const navbar = document.querySelector('.not-navbar');
+
+//Menu when not active
+let hamburgerBars = document.querySelectorAll('.hamburger-bar');
+console.log(hamburgerBars);
+
+//Menu when active
+let hamburgerToggle = document.createElement('div');
+hamburgerToggle.classList.add('hamburger-menu.active');
 
 hamburgerMenu.addEventListener('click', () => {
+  if (navbar.classList.contains('hidden')) {
+    navbar.classList.remove('hidden');   
+  } else {
+    navbar.classList.add('hidden');
+    hamburgerMenu.setAttribute('visibility', 'visible');
+  }
+
+  if (hamburgerNavMenu.classList.contains('hidden')) {
+    hamburgerNavMenu.classList.remove('hidden');
+
+  } else {
+    hamburgerNavMenu.classList.add('hidden');
+    
+  }
   hamburgerMenu.classList.toggle('active');
   hamburgerNavMenu.classList.toggle('active');
-  // hamburgerNavMenu.classList.toggle("showHamburgerNavMenu");
+  hamburgerNavMenu.classList.toggle("showHamburgerNavMenu");
 });
-
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburgerMenu.classList.remove('active');
-  hamburgerNavMenu.classList.remove('active');
-  // hamburgerNavMenu.classList.remove("showHamburgerNavMenu");
-}));
